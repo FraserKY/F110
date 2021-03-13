40e2f7a14c6bf9b3f24c4af5f174cee798911618
FraserKY <fraserkearsey@gmail.com>
2021-03-13 18:55:57 +0000

    Added new custom message, CMakeFile reporting error

--- devel/share/f1tenth_simulator/cmake/f1tenth_simulator-msg-paths.cmake
+++ devel/share/f1tenth_simulator/cmake/f1tenth_simulator-msg-paths.cmake
@@ -1,4 +1,4 @@
 # generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
 
-set(f1tenth_simulator_MSG_INCLUDE_DIRS "")
-set(f1tenth_simulator_MSG_DEPENDENCIES std_msgs;nav_msgs;sensor_msgs)
+set(f1tenth_simulator_MSG_INCLUDE_DIRS "/home/fraser/catkin_ws/src/f1tenth_simulator/msg")
+set(f1tenth_simulator_MSG_DEPENDENCIES std_msgs;nav_msgs;sensor_msgs;scan_range)
