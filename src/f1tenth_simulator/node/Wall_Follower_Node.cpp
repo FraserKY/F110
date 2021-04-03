3acf15d5c6408f3f79e241e17e160da08b4dc1a8
Fraser Kearsey <fraserkearsey@gmail.com>
2021-04-03 12:00:03 +0100

    New wall follower node

--- src/f1tenth_simulator/node/Wall_Follower_Node.cpp
+++ src/f1tenth_simulator/node/Wall_Follower_Node.cpp
@@ -12,6 +12,10 @@ class Wall_Follower{
 private:
     ros::NodeHandle n;
 
+    // Get Topic Names
+    std::string laser_scan_topic;
+    n.getParam("scan_topic", laser_scan_topic);
+
     // Create Subscribers
     ros::Subscriber LaserScan;
 
@@ -19,20 +23,32 @@ private:
 
     // TODO: Precompute cos angle of LS data
 
+    //
+
 public:
     Wall_Follower(){
         // Start Node
         n = ros::NodeHandle("~");
 
         // TODO: Set up subscribers and publishers
+        LaserScan = n.subscribe(laser_scan_topic, 1, &Wall_Follower::LS_callback, this);
 
-
-
+        DriveMessage = n.advertise<ackermann_msgs::AckermannDriveStamped>("/nav", 1)
 
         // Define any const variables
 
         const int Angle_A = 60;
         const double Angle_A_Rad = Angle_A * (M_PI / 180);
+
+        const double Kp = 0.0;
+        const double Ki = 0.0;
+        const double Kd = 0.0;
+
+        double integral_err = 0.0;
+        ros::Time prev_time = 0.0;
+        ros::Time current_time;
+
+        bool Once = true;
 
         // Distance from wall setpoint
         const double SetPoint = 1;
@@ -45,15 +61,13 @@ public:
     LS_callback(const sensor_msgs::LaserScan::ConstPtr &scan_msg){
 
         // Obtain LS distance at exactly East of Car (B), and 60 degrees anticlockwise of this point (A)
-
-        double B = scan_msg->Ranges[?];
-        double A = scan_msg->Ranges[?];
+        // Work out at which index 0 and 60 degrees lies
+        double B = scan_msg->Ranges[810];
+        double A = scan_msg->Ranges[(toRadians(60) / scan_msg->angle_increment) + 810];
 
         // Calculate Alpha (Alpha), the angle between exactly east of the car, and the shortest distance to the wall
 
-        double alpha;
-
-        alpha = atan(   (A * cos(Angle_A) - B)   /    (A * sin(Angle_A)) );   // Result in radians
+        double alpha = std::atan(   (A * cos(Angle_A_Rad) - B)   /    (A * sin(Angle_A_Rad)) );   // Result in radians
 
         // Calculate the distance of the LIDAR sensor from the wall (Dt)
 
@@ -71,11 +85,47 @@ public:
 
         double Error = SetPoint - Dt_1;
 
+        // Set first prev_time
+        if(Once == true){
+            ros::Time prev_time = scan_msg->header;
+            Once = false;
+        }
+
         // Call PID Function
-        // TODO: Write PID Function
+        double U_t = PID_Calc(Error);
 
         // Call Publish Function
         // TODO: Write Publish function
+
+    }
+
+    double PID_Calc(double &error){
+
+        // Get Current Time
+        ros::Time current_time = ros::Time::now();
+
+        // Calculate dt
+        double dt = current_time - prev_time;
+
+        // Sum Integral
+        integral_err += error * dt;
+
+        // Calculate Control Effort (Steering Angle)
+        double U_t = Kp * error + Ki * integral_err;
+
+        // Set prev_time equal to cur_time
+        prev_time = current_time;
+
+        // Return Control Output
+        return U_t;
+
+    }
+
+    double car_speed(double *steering_angle){
+
+    }
+
+    void publish_command(double *speed, double *steering_angle){
 
     }
 
@@ -86,7 +136,7 @@ public:
 
 int main(int argc, char ** argv) {
     ros::init(argc, argv, "wall_follower");
-    Safety wall_follower;
+    Wall_Follower wf;
     ros::spin();
     return 0;
 }
