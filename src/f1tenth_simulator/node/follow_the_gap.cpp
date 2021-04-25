8f2bfb477d2f8186ddd0e7afbbfc815f83579ff4
Fraser Kearsey <fraserkearsey@gmail.com>
2021-04-25 13:19:35 +0100

    Set Speed back to dynamically determined

--- src/f1tenth_simulator/node/follow_the_gap.cpp
+++ src/f1tenth_simulator/node/follow_the_gap.cpp
@@ -71,7 +71,7 @@ public:
         // TODO: Function to determine speed based on steering angle
 
         drive_msg.steering_angle = steering_angle;
-        drive_msg.speed = 1.0;//DetSpeed(steering_angle);
+        drive_msg.speed = DetSpeed(steering_angle);
 
        // ROS_INFO_STREAM("Speed: " << drive_msg.speed);
 
